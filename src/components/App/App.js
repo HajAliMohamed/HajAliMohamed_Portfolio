056f0171ef347268d01f92b43fb219b398d24617
HajAliMohamed <mohamedhajali45@gmail.com>
2022-11-24 09:25:33 +0100

    change API url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import About from "../About/About";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
