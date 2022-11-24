f26c731be53af2c56358b9f8636f83883739de1a
HajAliMohamed <mohamedhajali45@gmail.com>
2022-11-24 21:41:53 +0100

    add HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import "./style/App.scss";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import Portfolio from "../Portfolio/Portfolio";
 import Contact from "../Contact/Contact";
@@ -7,14 +7,14 @@ import About from "../About/About";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
